fd80f9d811e5a974792e40419983a7fc8a4ff0c3
KyrylIeremieiev <34677@ma-web.nl>
2023-07-18 15:21:49 +0200

    add db connection, add table

--- simpleScript.py
+++ simpleScript.py
@@ -1,5 +1,16 @@
-number = 1
-if number == 1:
-    print(1)
-else:
-    print(2)
+import mysql.connector as connector
+
+conn = connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="pythonDB")
+
+sql = "CREATE TABLE relics (name VARCHAR(225), expansion VARCHAR(20));"
+
+mycursor = conn.cursor()
+
+mycursor.execute(sql)
+
+#comman line program with class start asks for
+#create if not exist
