af8537b4bff0d196ba6008e48ad9f4fe5574f428
KyrylIeremieiev <34677@ma-web.nl>
2023-07-18 16:21:11 +0200

    add class, add modules, add select, insert and drop query

--- simpleScript.py
+++ simpleScript.py
@@ -1,16 +1,65 @@
 import mysql.connector as connector
 
-conn = connector.connect(
-    host="localhost",
-    user="root",
-    password="",
-    database="pythonDB")
+class DB:
+    def __init__(self):
+        print("\033c", end="")
+        self.conn = connector.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="pythondb")
 
-sql = "CREATE TABLE relics (name VARCHAR(225), expansion VARCHAR(20));"
+        sql = "CREATE TABLE if not exists relics (name VARCHAR(225), expansion VARCHAR(20));"
 
-mycursor = conn.cursor()
+        self.mycursor = self.conn.cursor()
 
-mycursor.execute(sql)
+        self.mycursor.execute(sql)
+
+
+        self.choose_action()
+        self.controller()
+
+    def choose_action(self):
+        print("What action would you like to take? (select/insert/drop)")
+        self.action = input()
+        print("What table?")
+        self.table = input()
+
+    def controller(self):
+        if self.action == "select" or self.action == "SELECT":
+            self.select_query()
+        if self.action == "insert" or self.action == "INSERT":
+            self.insert_query()
+        if self.action == "drop" or self.action == "DROP":
+            self.drop_query()
+
+
+    def select_query(self):
+        print("\033c", end="")
+        sql = "SELECT * FROM {};".format(self.table)
+        self.mycursor.execute(sql)
+        myresult = self.mycursor.fetchall()
+        for x in myresult:
+            print(x)
+
+    def insert_query(self):
+        print("\033c", end="")
+        print("What arguments do you want to use?")
+        firstArgument = input()
+        secondArgument = input()
+        sql = "INSERT INTO {}(name, expansion) VALUES(%s, %s);".format(self.table)
+        val = (firstArgument, secondArgument)
+        self.mycursor.execute(sql, val)
+
+        self.conn.commit()
+
+    def drop_query(self):
+        print("\033c", end="")
+        sql = "DROP TABLE {};".format(self.table)
+        self.mycursor.execute(sql)
+
+
+p1 = DB()
 
 #comman line program with class start asks for
 #create if not exist
