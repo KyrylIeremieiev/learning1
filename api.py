a2270551109a18483caca800b8861fd42b903296
KyrylIeremieiev <34677@ma-web.nl>
2023-08-19 20:49:10 +0200

    add insert function

--- api.py
+++ api.py
@@ -3,7 +3,9 @@ import mysql.connector as connector
 import bottle_mysql
 from bottle import route, run
 from bottle import error
+from bottle import response
 from bottle import template
+from bottle import request
 import json
 app = bottle.Bottle()
 plugin = bottle_mysql.Plugin(dbuser='root', dbpass='', dbname='pythondb')
@@ -15,7 +17,21 @@ db = connector.connect(
     database="pythondb")
 mycursor = db.cursor()
 
+def enable_cors(fn):
+    def _enable_cors(*args, **kwargs):
+        # set CORS headers
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
+        response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+
+        if bottle.request.method != 'OPTIONS':
+            # actual request; reply with the actual response
+            return fn(*args, **kwargs)
+
+    return _enable_cors
+
 @route('/show/:item')
+@enable_cors
 def show(item):
     sql = "SELECT * FROM relics where name ='{}';".format(item)
     mycursor.execute(sql)
@@ -24,6 +40,16 @@ def show(item):
         myresult = json.dumps(myresult)
         return myresult
     return error404
+
+@route('/insert')
+@enable_cors
+def insert():
+    data = request.get_json()
+    name = data['name']
+    expansion = data['expansion']
+    sql = "INSERT INTO relics (name, expansion) VALUES('{}', '{}');".format(name, expansion)
+    mycursor.execute(sql)
+
 
 @route('/hello')
 def hello():
